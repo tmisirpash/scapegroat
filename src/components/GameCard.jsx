878bfc78ee59cc26c79a71d39cf7b872f7659fa4
Timur Misirpashayev <tmisirpash@system76-pc.localdomain>
2022-07-09 20:19:53 -0700

    add cards and transition

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box} from '@mui/material';
+import '../App.css';
+
+
+export function GameCard(props) {
+    return (
+        <Box className="card card-node">
+            <span>Game</span>
+        </Box>
+    );
+}
