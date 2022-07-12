c2e8fd25789e832f9374a00e5e498c4736f99eb7
Timur Misirpashayev <tmisirpash@system76-pc.localdomain>
2022-07-11 22:37:43 -0700

    button highlighting when mousing over

--- src/components/CardButton.jsx
+++ src/components/CardButton.jsx
@@ -3,8 +3,16 @@ import React from 'react';
 
 
 export function CardButton(props) {
+
+    const [backgroundColor, setBackgroundColor ]  = React.useState('#161522');
+
     return (
-        <div className="control-panel">
+        <div 
+            className="control-panel" 
+            style={{backgroundColor: backgroundColor}}
+            onMouseEnter={() => setBackgroundColor('#7672a8')}
+            onMouseLeave={() => setBackgroundColor('#161522')}
+        >
             <img src={props.img} height={50} width={50}></img>
             <span style={{paddingLeft: "8px", fontSize: props.fontSize}}>{props.text}</span>
             <img src={require('../images/arrow-icon.png')} height={50} width={50}></img>
