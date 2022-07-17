22a3466cb594a005815ce54c64421a31289cb97e
Timur Misirpashayev <tmisirpash@system76-pc.localdomain>
2022-07-16 18:40:04 -0700

    remove known private key

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,8 +10,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-const KNOWN_PRIVATE_KEY = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
-
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
